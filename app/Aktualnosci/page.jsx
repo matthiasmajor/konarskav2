9dd1856047aa8511fc43067492f1a4c8800ccc43
Hubert Majewski <130083088+matthiasmajor@users.noreply.github.com>
2024-10-30 20:05:25 +0100

    sam nie wiem

--- app/Aktualnosci/page.jsx
+++ app/Aktualnosci/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { client } from "@/lib/contentful";
 import Aktualnosci_sub from "@/components/Aktualnosci_sub";
--- app/lib/contentful.js
+++ app/lib/contentful.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { createClient } from "contentful";
 
 export const client = createClient({
