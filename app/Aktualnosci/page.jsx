9e5141fd3a75c111c7bf4fbe0dd4904ce7f12d71
Hubert Majewski <130083088+matthiasmajor@users.noreply.github.com>
2024-10-30 19:34:00 +0100

    change use client

--- app/Aktualnosci/page.jsx
+++ app/Aktualnosci/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { client } from "@/lib/contentful";
 import Aktualnosci_sub from "@/components/Aktualnosci_sub";
--- app/lib/contentful.js
+++ app/lib/contentful.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { createClient } from "contentful";
 
 export const client = createClient({
